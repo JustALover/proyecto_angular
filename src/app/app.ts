faef0341a8418b9dae8e4cf8268ecf7f68d8bcea
sebastian <joansebastianrojassilva@gmail.com>
2025-10-01 12:18:30 -0500

    actualizacion de taller

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TareasNuevas } from './tareas-nuevas/tareas-nuevas';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TareasNuevas],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
